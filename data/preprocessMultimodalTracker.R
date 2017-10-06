b89eb8ee316b406873e88fdc9534a89916bfca7a
Luis P <luis.prieto@epfl.ch>
2017-10-06 09:31:14 +0300

    Initial datafile reading

--- data/preprocessMultimodalTracker.R
+++ data/preprocessMultimodalTracker.R
@@ -1,10 +1,15 @@
 library(jsonlite)
 library(plyr)
-# 
+
+# Reads a raw text file with the data from a multimodal tracker, 
+# and spits a list with dataframes with the different timestamped 
+# data streams
 preprocessMultimodalTracker <- function(rawdatafilename){
 
   #For tests only
   #rawdatafilename <- "C:\\Users\\lprisan\\workspace\\multimodal-tracker\\data\\multimodal-tracker-1507051956530.txt"
+  
+  #TODO: extract the device ID from the filename, and add it as a column
   
   # Read the file  
   lines <- paste(readLines(rawdatafilename),collapse="")
@@ -16,6 +21,8 @@ preprocessMultimodalTracker <- function(rawdatafilename){
   #This gets us a sparse dataframe
   #TODO: expand the beacon rows into multiple ones with beacon id and its data
 
+  #TODO: divide the whole dataframe in separate sources for different data streams
   
+  #TODO: return a list with the whole and separate dataframes
   
 }
